6fc4a34dc277be78a6628403ef06468490d5c96b
Ahmed Magdi <ammhaa2012@gmail.com>
2020-10-15 19:48:38 +0200

    added templates

---
+++ recruitmentsystem/controllers/email-templates.js
@@ -0,0 +1,48 @@
+exports.phaseAcceptanceEmail = {
+    body: {
+        greeting: "Dear Applicant, ",
+        signature: false,
+        intro: "You have been rejected bitch",
+        action: {
+            instructions: "To get started with Mailgen, please click here:",
+            button: {
+                color: "#22BC66", // Optional action button color
+                text: "Confirm your account",
+                link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO",
+            },
+        },
+        outro: "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+};
+exports.phaseRejectionEmail = {
+    body: {
+        greeting: "Dear Applicant, ",
+        signature: false,
+        intro: "You have been rejected bitch",
+        action: {
+            instructions: "To get started with Mailgen, please click here:",
+            button: {
+                color: "#22BC66", // Optional action button color
+                text: "Confirm your account",
+                link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO",
+            },
+        },
+        outro: "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+};
+exports.finalAcceptanceEmail = {
+    body: {
+        greeting: "Dear Applicant, ",
+        signature: false,
+        intro: "You have been rejected bitch",
+        action: {
+            instructions: "To get started with Mailgen, please click here:",
+            button: {
+                color: "#22BC66", // Optional action button color
+                text: "Confirm your account",
+                link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO",
+            },
+        },
+        outro: "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+};
