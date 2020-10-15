e76306e8c68a4a078919a7ae8555f6f8d06c59dc
Ahmed Magdi <ammhaa2012@gmail.com>
2020-10-15 19:58:13 +0200

    added delete events

--- recruitment system/controllers/event.js
+++ recruitment system/controllers/event.js
@@ -1,93 +1,92 @@
-const Event = require('../models/event');
-const Team = require('../models/team');
-const Subteam = require('../models/subteam');
+const Event = require("../models/event");
+const Team = require("../models/team");
+const Subteam = require("../models/subteam");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 exports.getAllEvents = (req, res, next) => {
     Event.find()
-    .populate('team', '-subteams')
-    .then(events => {
-        res.status(200).json({
-            message: 'all events fetched',
-            events: events
+        .populate("team", "-subteams")
+        .then((events) => {
+            res.status(200).json({
+                message: "all events fetched",
+                events: events,
+            });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
         });
-    })
-    .catch(err => {
-        if (!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    });
-}
+};
 
 exports.getEvent = (req, res, next) => {
     const eventId = req.params.eventId;
     Event.findById(eventId)
-    .populate('activeSubteams')
-    .populate({path: 'team', populate: {path: 'subteams', model: 'Subteam'}})
-    // {path: 'event', populate: {path: 'team', model: 'Team'}}
-    .then(event => {
-        if(!event){
-            const error = new Error('Could not find event');
-            error.statusCode = 404;
-            throw error;
-        }
+        .populate("activeSubteams")
+        .populate({ path: "team", populate: { path: "subteams", model: "Subteam" } })
+        // {path: 'event', populate: {path: 'team', model: 'Team'}}
+        .then((event) => {
+            if (!event) {
+                const error = new Error("Could not find event");
+                error.statusCode = 404;
+                throw error;
+            }
 
-        res.status(200).json({
-            message: "event fetched",
-            event: event
+            res.status(200).json({
+                message: "event fetched",
+                event: event,
+            });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
         });
-    })
-    .catch(err => {
-        if (!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    });
-}
+};
 
 exports.addNewEvent = (req, res, next) => {
     const teamId = mongoose.Types.ObjectId(req.body.teamId);
     const season = req.body.season;
     const eventActive = req.body.eventActive;
     const questions = req.body.questions;
-    
-    // TODO ::  
-    // CANT START AN EVENT FOR A TEAM IN A SEASON IF CURRENT TEAM 
+
+    // TODO ::
+    // CANT START AN EVENT FOR A TEAM IN A SEASON IF CURRENT TEAM
     // ALREADY HAS AN EVENT IN THIS SEASON - OR ASK KOMY
 
     Team.findById(teamId)
-    .then(team => {
-        if(!team){
-            const error = new Error('team doesnt exist');
-            error.statusCode = 404;
-            throw error;
-        }
-        // console.log("YAAAYAYA", team.subteams);
+        .then((team) => {
+            if (!team) {
+                const error = new Error("team doesnt exist");
+                error.statusCode = 404;
+                throw error;
+            }
+            // console.log("YAAAYAYA", team.subteams);
 
-        return new Event({
-            team: teamId,
-            season: season,
-            eventActive: eventActive,
-            questions: questions,
-            activeSubteams: team.subteams
+            return new Event({
+                team: teamId,
+                season: season,
+                eventActive: eventActive,
+                questions: questions,
+                activeSubteams: team.subteams,
+            }).save();
         })
-        .save();
-    })
-    .then(event => {
-        res.status(201).json({
-            message: 'event created!',
-            event: event
+        .then((event) => {
+            res.status(201).json({
+                message: "event created!",
+                event: event,
+            });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
         });
-    })
-    .catch(err => {
-        if (!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    });
-}
+};
 
 exports.updateEvent = (req, res, next) => {
     const eventId = req.params.eventId;
@@ -101,202 +100,223 @@ exports.updateEvent = (req, res, next) => {
     const phase = req.body.currentPhase;
 
     Event.findById(eventId)
-    .then(event => {
-        if(!event){
-            const error = new Error('Could not find event');
-            error.statusCode = 404;
-            throw error;
-        }
+        .then((event) => {
+            if (!event) {
+                const error = new Error("Could not find event");
+                error.statusCode = 404;
+                throw error;
+            }
 
-        event.team = teamId;
-        event.season = season;
-        event.eventActive = eventActive;
-        event.questions = questions;
-        event.activeSubteams = activeSubteams;
-        event.currentPhase = phase;
+            event.team = teamId;
+            event.season = season;
+            event.eventActive = eventActive;
+            event.questions = questions;
+            event.activeSubteams = activeSubteams;
+            event.currentPhase = phase;
 
-        return event.save();
-    })
-    .then(event => {
-        res.status(200).json({
-            message: 'event updated',
-            event: event
+            return event.save();
+        })
+        .then((event) => {
+            res.status(200).json({
+                message: "event updated",
+                event: event,
+            });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
         });
-    })
-    .catch(err => {
-        if (!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    });
-}
+};
 
 exports.toggleEventStatus = (req, res, next) => {
     const eventId = req.params.eventId;
 
     Event.findById(eventId)
-    .then(event => {
-        if(!event){
-            const error = new Error('Could not find event');
-            error.statusCode = 404;
-            throw error;
-        }
+        .then((event) => {
+            if (!event) {
+                const error = new Error("Could not find event");
+                error.statusCode = 404;
+                throw error;
+            }
 
-        event.eventActive = !event.eventActive;
-        return event.save();
-    })
-    .then(event => {
-        res.status(200).json({
-            message: 'event status toggled',
-            event: event
+            event.eventActive = !event.eventActive;
+            return event.save();
+        })
+        .then((event) => {
+            res.status(200).json({
+                message: "event status toggled",
+                event: event,
+            });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
         });
-    })
-    .catch(err => {
-        if (!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    });
-}
+};
 
 exports.incrementNumApplicants = (req, res, next) => {
     const eventId = req.params.eventId;
 
     Event.findById(eventId)
-    .then(event => {
-        if(!event){
-            const error = new Error('Could not find event');
-            error.statusCode = 404;
-            throw error;
-        }
+        .then((event) => {
+            if (!event) {
+                const error = new Error("Could not find event");
+                error.statusCode = 404;
+                throw error;
+            }
 
-        event.numApplicants += 1;
-        return event.save();
-    })
-    .then(event => {
-        res.status(200).json({
-            message: 'event incremented number of applicants!',
-            event: event
+            event.numApplicants += 1;
+            return event.save();
+        })
+        .then((event) => {
+            res.status(200).json({
+                message: "event incremented number of applicants!",
+                event: event,
+            });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
         });
-    })
-    .catch(err => {
-        if (!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    });
-}
+};
 
 exports.incrementNumAccepted = (req, res, next) => {
     const eventId = req.params.eventId;
 
     Event.findById(eventId)
-    .then(event => {
-        if(!event){
-            const error = new Error('Could not find event');
-            error.statusCode = 404;
-            throw error;
-        }
+        .then((event) => {
+            if (!event) {
+                const error = new Error("Could not find event");
+                error.statusCode = 404;
+                throw error;
+            }
 
-        event.numAccepted += 1;
-        return event.save();
-    })
-    .then(event => {
-        res.status(200).json({
-            message: 'event incremented number of accepted!',
-            event: event
+            event.numAccepted += 1;
+            return event.save();
+        })
+        .then((event) => {
+            res.status(200).json({
+                message: "event incremented number of accepted!",
+                event: event,
+            });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
         });
-    })
-    .catch(err => {
-        if (!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    });
-}
+};
 
 exports.incrementNumRejected = (req, res, next) => {
     const eventId = req.params.eventId;
 
     Event.findById(eventId)
-    .then(event => {
-        if(!event){
-            const error = new Error('Could not find event');
-            error.statusCode = 404;
-            throw error;
-        }
+        .then((event) => {
+            if (!event) {
+                const error = new Error("Could not find event");
+                error.statusCode = 404;
+                throw error;
+            }
 
-        event.numRejected += 1;
-        return event.save();
-    })
-    .then(event => {
-        res.status(200).json({
-            message: 'event incremented number of rejected!',
-            event: event
+            event.numRejected += 1;
+            return event.save();
+        })
+        .then((event) => {
+            res.status(200).json({
+                message: "event incremented number of rejected!",
+                event: event,
+            });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
         });
-    })
-    .catch(err => {
-        if (!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    });
-}
+};
 
 exports.incrementNumPendAcc = (req, res, next) => {
     const eventId = req.params.eventId;
 
     Event.findById(eventId)
-    .then(event => {
-        if(!event){
-            const error = new Error('Could not find event');
-            error.statusCode = 404;
-            throw error;
-        }
+        .then((event) => {
+            if (!event) {
+                const error = new Error("Could not find event");
+                error.statusCode = 404;
+                throw error;
+            }
 
-        event.numPendAcc += 1;
-        return event.save();
-    })
-    .then(event => {
-        res.status(200).json({
-            message: 'event incremented number of pending acc!',
-            event: event
+            event.numPendAcc += 1;
+            return event.save();
+        })
+        .then((event) => {
+            res.status(200).json({
+                message: "event incremented number of pending acc!",
+                event: event,
+            });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
         });
-    })
-    .catch(err => {
-        if (!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    });
-}
+};
 
 exports.incrementNumPendRej = (req, res, next) => {
     const eventId = req.params.eventId;
 
     Event.findById(eventId)
-    .then(event => {
-        if(!event){
-            const error = new Error('Could not find event');
-            error.statusCode = 404;
-            throw error;
-        }
-        
-        event.numPendRej += 1;
-        return event.save();
-    })
-    .then(event => {
-        res.status(200).json({
-            message: 'event incremented number of pending rejection!',
-            event: event
-        });
-    })
-    .catch(err => {
-        if (!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    });
-}
+        .then((event) => {
+            if (!event) {
+                const error = new Error("Could not find event");
+                error.statusCode = 404;
+                throw error;
+            }
 
-exports.deleteEvent = (req, res, next) => {}
+            event.numPendRej += 1;
+            return event.save();
+        })
+        .then((event) => {
+            res.status(200).json({
+                message: "event incremented number of pending rejection!",
+                event: event,
+            });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
+
+exports.deleteEvent = (req, res, next) => {
+    const eventId = req.params.eventId;
+
+    Event.findById(eventId)
+        .then((event) => {
+            if (!event) {
+                const error = new Error("Could not find event");
+                error.statusCode = 404;
+                throw error;
+            }
+            return Event.findByIdAndDelete(eventId);
+        })
+        .then((result) => {
+            res.status(200).json({ message: "Deleted event." });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
--- recruitment system/routes/event.js
+++ recruitment system/routes/event.js
@@ -1,34 +1,54 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 
-const eventsController = require('../controllers/event');
+const eventsController = require("../controllers/event");
 
-const isAdmin = require('../../middleware/isAdmin');
-const isAuth = require('../../middleware/isAuth');
+const isAdmin = require("../../middleware/isAdmin");
+const isAuth = require("../../middleware/isAuth");
 
 const router = express.Router();
 
-router.get('/all-events', isAuth, isAdmin(2), eventsController.getAllEvents);
+router.get("/all-events", isAuth, isAdmin(2), eventsController.getAllEvents);
 
-router.get('/get-event/:eventId', isAuth, isAdmin(2), eventsController.getEvent);
+router.get("/get-event/:eventId", isAuth, isAdmin(2), eventsController.getEvent);
 
-router.post('/add-event', isAuth, isAdmin(3), eventsController.addNewEvent);
+router.post("/add-event", isAuth, isAdmin(3), eventsController.addNewEvent);
 
-router.put('/update-event/:eventId', isAuth, isAdmin(2), eventsController.updateEvent);
+router.put("/update-event/:eventId", isAuth, isAdmin(2), eventsController.updateEvent);
 
-router.put('/edit-event-status/:eventId', isAuth, isAdmin(2), eventsController.toggleEventStatus);
+router.put("/edit-event-status/:eventId", isAuth, isAdmin(2), eventsController.toggleEventStatus);
 
-router.put('/increment-event-applicants/:eventId',  isAuth, eventsController.incrementNumApplicants);
+router.put("/increment-event-applicants/:eventId", isAuth, eventsController.incrementNumApplicants);
 
-router.put('/increment-num-acc/:eventId', isAuth, isAdmin(2), eventsController.incrementNumAccepted);
+router.put(
+    "/increment-num-acc/:eventId",
+    isAuth,
+    isAdmin(2),
+    eventsController.incrementNumAccepted
+);
 
-router.put('/increment-num-rej/:eventId', isAuth, isAdmin(2), eventsController.incrementNumRejected);
+router.put(
+    "/increment-num-rej/:eventId",
+    isAuth,
+    isAdmin(2),
+    eventsController.incrementNumRejected
+);
 
-router.put('/increment-num-pendacc/:eventId', isAuth, isAdmin(2), eventsController.incrementNumPendAcc);
+router.put(
+    "/increment-num-pendacc/:eventId",
+    isAuth,
+    isAdmin(2),
+    eventsController.incrementNumPendAcc
+);
 
-router.put('/increment-num-pendrej/:eventId', isAuth, isAdmin(2), eventsController.incrementNumPendRej);
+router.put(
+    "/increment-num-pendrej/:eventId",
+    isAuth,
+    isAdmin(2),
+    eventsController.incrementNumPendRej
+);
 
-// TODO :: EVENT DELETION
+router.delete("/delete-event/:eventId", isAuth, isAdmin(2), eventsController.deleteEvent);
 
 module.exports = router;
 
